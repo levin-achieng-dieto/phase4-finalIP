85bd2f54c47b2f60bc29211d3f5ac68d8d45dda3
ManyweleMike <mugendimike80@gmail.com>
2022-12-06 12:27:09 +0300

    airlines seeds done

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,28 @@
 airlines = Airline.create([
-    {
+    { 
         name: "United Airlines",
-    image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
-},
-{
-    name: 'Southwest',
-    image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
-},
-{
-    name: "rwanda Airlines",
-image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
-},
+        image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+      }, 
+      { 
+        name: "Southwest",
+        image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
+      },
+      { 
+        name: "Delta",
+        image_url: "https://open-flights.s3.amazonaws.com/Delta.png" 
+      }, 
+      { 
+        name: "Alaska Airlines",
+        image_url: "https://open-flights.s3.amazonaws.com/Alaska-Airlines.png" 
+      }, 
+      { 
+        name: "JetBlue",
+        image_url: "https://open-flights.s3.amazonaws.com/JetBlue.png" 
+      }, 
+      { 
+        name: "American Airlines",
+        image_url: "https://open-flights.s3.amazonaws.com/American-Airlines.png" 
+      }
     
 ])
 
