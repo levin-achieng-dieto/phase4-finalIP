4ac7854a65a565f6b37a63459125140d4bb20093
ManyweleMike <mugendimike80@gmail.com>
2022-12-06 12:21:25 +0300

    reviews done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,12 @@ airlines = Airline.create([
 {
     name: 'Southwest',
     image_url: "https://open-flights.s3.amazonaws.com/Southwest-Airlines.png"
-}
+},
+{
+    name: "rwanda Airlines",
+image_url: "https://open-flights.s3.amazonaws.com/United-Airlines.png"
+},
+    
 ])
 
 reviews = Review.create([
@@ -21,5 +26,54 @@ reviews = Review.create([
         description: "i had a bad time",
         score: 1,
         airline: airlines.first
-    }
+    },
+    {
+        title: 'Good',
+        description: 'Averege',
+        score: 6,
+        airline: airlines.first
+    },
+    {
+        title: 'Great ',
+        description: 'good food.',
+        score: 8,
+        airline: airlines.first
+    },
+    {
+        title: 'Horrible',
+        description: 'Rude air crew.',
+        score: 3,
+        airline: airlines.first
+    },
+    {
+        title: 'Good',
+        description: 'enjoyed',
+        score: 5,
+        airline: airlines.first
+    },
+    {
+        title: 'Awesome',
+        description: 'nice travel time.',
+        score: 7,
+        airline: airlines.first
+    },
+    {
+        title: 'averege',
+        description: 'lovely time.',
+        score: 4,
+        airline: airlines.first
+    },
+    {
+        title: 'Terrible',
+        description: 'Bad food.',
+        score: 2,
+        airline: airlines.first
+    },
+    {
+        title: 'can do better',
+        description: 'lost luggage',
+        score: 6,
+        airline: airlines.first
+    },
+
 ])
